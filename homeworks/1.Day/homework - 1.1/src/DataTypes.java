c8054d9b1abaac40f4e81c0edba876908b171e19
Sahil Rzayev <sahilrzayev2000@gmail.com>
2021-04-30 22:22:09 +0400

    add homework - 1.1

---
+++ homeworks/1.Day/homework - 1.1/src/DataTypes.java
@@ -0,0 +1,42 @@
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		/*
+		Java’da veri tipleri 3 gruba ayrýlýr:
+		1.Ýlkel (Primitive) Veri Tipleri
+		2.Referans Tipleri
+		3.Null Tipi
+		
+		
+		
+		Ýlkel Veri Tipleri
+		a)Tam sayýlar:
+		Byte: -128 ve 127 arasýnda deðerler alýr. (8 bit)
+		Short: -32768 ve 32767 arasýnda deðerler alýr. (16 bit)
+		Integer: Orta uzunluktaki sayýlarý tutmak için kullanýlýr. En fazla kullanýlan veri tipidir. (32 bit)
+		Long: Çok uzun sayýlarý tutmak için kullanýlýr. (64 bit)
+		
+		b)Ondalýklý Sayýlar:
+		Float: Bu tip virgülden sonra 7 haneye kadar yeterli duyarlýlýktadýr. (Tek duyarlý — 32 bit)
+		Double: Bu tip virgülden sonra 17 haneye kadar yeterli duyarlýlýktadýr. (Çift duyarlý — 64 bit)
+		Char: Javada karakterleri temsil eder. 16 bit yer kaplar.
+		Boolean: Bu tip sadece True-False deðerini alabilir.
+		
+		
+		
+		Referans Tipleri:
+		a)Class
+		b)Interface
+		c)Array
+		
+		
+		
+		Null Tipi
+		Null - herhangi bir deðere sahip olmama durumuna verilen isimdir.
+		
+		*/
+
+	}
+
+}
