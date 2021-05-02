a0fb4c871644b1fed2bc12e180e590fea35bc67b
Sahil Rzayev <sahilrzayev2000@gmail.com>
2021-05-02 16:03:47 +0400

    add oopIntro

--- oopIntro/src/oopIntro/Main.java
+++ oopIntro/src/oopIntro/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Product product1 = new Product(1,"Lenovo V14",15000,"16 GB Ram");// örnek oluþturma, referans oluþturma, instance
-		
+		Product product1 = new Product(1, "Lenovo V14", 15000, "16 GB Ram");// örnek oluþturma, referans oluþturma,
+																			// instance
 
 		Product product2 = new Product();
 		product2.id = 2;
@@ -19,6 +19,8 @@ public class Main {
 		product3.unitPrice = 10000;
 		product3.detail = "8 GB Ram";
 
+		
+		
 		Product[] products = { product1, product2, product3 };
 
 		for (Product product : products) {
@@ -26,16 +28,23 @@ public class Main {
 		}
 
 		System.out.println(products.length);
+
+		Category category1 = new Category();
+		category1.id = 1;
+		category1.name = "Bilgisayar";
+
+		Category category2 = new Category();
+		category2.id = 2;
+		category2.name = "Ev/Bahçe";
+
 		
 		
-		
-		Category category1=new Category();
-		category1.id=1;
-		category1.name="Bilgisayar";
-		
-		Category category2=new Category();
-		category2.id=2;
-		category2.name="Ev/Bahçe";
+		ProductManager productManager = new ProductManager();
+		productManager.addToCart(product1);
+
+		productManager.addToCart(product2);
+
+		productManager.addToCart(product3);
 
 	}
 
--- oopIntro/src/oopIntro/ProductManager.java
+++ oopIntro/src/oopIntro/ProductManager.java
@@ -1,5 +1,7 @@
 package oopIntro;
 
 public class ProductManager {
-
+	public void addToCart(Product product) {
+		System.out.println("Sepete eklendi : " + product.name);
+	}
 }
