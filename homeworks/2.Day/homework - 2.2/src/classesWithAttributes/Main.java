85ad1a3474350be7a398297307936eccee1acafd
Sahil Rzayev <sahilrzayev2000@gmail.com>
2021-05-04 07:04:04 +0400

    add homework - 2.2

--- homeworks/2.Day/homework - 2.2/src/classesWithAttributes/Main.java
+++ homeworks/2.Day/homework - 2.2/src/classesWithAttributes/Main.java
@@ -3,16 +3,19 @@ package classesWithAttributes;
 public class Main {
 
 	public static void main(String[] args) {
-		Product product = new Product();
-		product.name = "Laptop";
-		product.id = 1;
-		product.description = "Asus Laptop";
-		product.price = 5000;
-		product.stockAmount = 3;
-
+		Product product =new Product(1,"Laptop","Asus Laptop",3000,2,"Siyah");
+		
+		/*Product product = new Product();
+		product.setName("Laptop");
+		product.setId(1);
+		product.setDescription("Asus Laptop");
+		product.setPrice(5000);
+		product.setStockAmount(3);*/
+		
 		ProductManager productManager = new ProductManager();
 		productManager.Add(product);
 
+		System.out.println(product.getKod()); // L1
 	}
 
 }
--- homeworks/2.Day/homework - 2.2/src/classesWithAttributes/ProductManager.java
+++ homeworks/2.Day/homework - 2.2/src/classesWithAttributes/ProductManager.java
@@ -2,7 +2,11 @@ package classesWithAttributes;
 
 public class ProductManager {
 	public void Add(Product product) {
-		// JDBC 
-		System.out.println("Ürün eklendi : " + product.name);
+		// JDBC
+		System.out.println("Ürün eklendi : " + product.getName());
+	}
+
+	public void Add2(int id, String name, String description, int stockAmount, double price, String renk) {
+
 	}
 }
