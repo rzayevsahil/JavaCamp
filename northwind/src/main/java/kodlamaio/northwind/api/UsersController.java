86522986d08bf9b227986f441556ab8ba7ce4c34
Sahil Rzayev <sahilrzayev2000@gmail.com>
2021-05-31 02:42:40 +0400

    update

--- northwind/src/main/java/kodlamaio/northwind/api/UsersController.java
+++ northwind/src/main/java/kodlamaio/northwind/api/UsersController.java
@@ -1,14 +1,27 @@
 package kodlamaio.northwind.api;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 
 import kodlamaio.northwind.business.abstracts.UserService;
 import kodlamaio.northwind.core.entities.User;
+import kodlamaio.northwind.core.utilities.results.ErrorDataResult;
 import kodlamaio.northwind.core.utilities.results.Result;
 
 @RestController
@@ -17,13 +30,27 @@ public class UsersController {
 
 	private UserService userService;
 
+	@Autowired
 	public UsersController(UserService userService) {
 		super();
 		this.userService = userService;
 	}
 	
 	@PostMapping(value="/add")
-	public ResponseEntity<?>  add(@RequestBody User user) {
+	public ResponseEntity<?>  add(@Valid @RequestBody User user) {
 		return ResponseEntity.ok(this.userService.add(user));
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ErrorDataResult<Object> handleValidationException(MethodArgumentNotValidException exceptions){
+		Map<String,String> validationErrors = new HashMap<String, String>();
+		
+		for(FieldError fieldError: exceptions.getBindingResult().getFieldErrors()) {
+			validationErrors.put(fieldError.getField(),fieldError.getDefaultMessage());			
+		}
+		
+		ErrorDataResult<Object> errors=new ErrorDataResult<Object>(validationErrors,"Doğrulama hataları");
+		return errors;
 	}
 }
--- northwind/src/main/java/kodlamaio/northwind/business/concretes/UserManager.java
+++ northwind/src/main/java/kodlamaio/northwind/business/concretes/UserManager.java
@@ -1,5 +1,7 @@
 package kodlamaio.northwind.business.concretes;
 
+import org.springframework.stereotype.Service;
+
 import kodlamaio.northwind.business.abstracts.UserService;
 import kodlamaio.northwind.core.dataAccess.UserDao;
 import kodlamaio.northwind.core.entities.User;
@@ -8,6 +10,7 @@ import kodlamaio.northwind.core.utilities.results.Result;
 import kodlamaio.northwind.core.utilities.results.SuccessDataResult;
 import kodlamaio.northwind.core.utilities.results.SuccessResult;
 
+@Service
 public class UserManager implements UserService {
 
 	private UserDao userDao;
